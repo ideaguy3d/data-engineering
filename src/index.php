9318e9579dc9c07e6e6194b55dbbf9e1a6f5e294
Julius Alvarado <phpninja@mail.com>
2020-03-24 22:36:38 -0700

    scanning csv, basic architecture set up

--- src/index.php
+++ src/index.php
@@ -1,1 +1,29 @@
 <?php
+declare(strict_types=1);
+/**
+ * Created by PhpStorm.
+ * User: julius hernandez alvarado
+ * Date: 3/23/2020
+ * Time: 8:46 PM
+ *
+ * Insight coding challenge built with PHP 7.4
+ *
+ * branch qb
+ */
+
+use dataphp\ProcessComplaints;
+
+// manually require classes
+require 'classes/DataStream.php';
+require 'classes/ProcessComplaints.php';
+
+$pathToCsv = '../input/complaints.csv';
+
+$complaintProcessor = new ProcessComplaints($pathToCsv);
+
+// let's see how much memory and time is required just to scan the data with no app logic
+$startTime = microtime(true);
+$m = number_format($complaintProcessor->compute());
+$endTime = number_format($startTime - microtime(true));
+echo "\nTotal time to run program = $endTime\n";
+echo "\nMost memory used = $m\n";
